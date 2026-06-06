9e53dde4321d34c3509e02c6b855092c2284da24
霍依尤克 <857464946@qq.com>
2026-06-07 00:09:37 +0800

    Update 115-config.js

--- 115-config.js
+++ 115-config.js
@@ -7,7 +7,7 @@ window.YUNLANG_115_CONFIG = {
   apiBaseUrl: "http://localhost:8787",
 
   // 115 授权后得到的 refresh token。正式桌面版建议改成系统钥匙串或本地加密存储。
-  refreshToken: "4fkn2.c09eefacdde5403b99bc5f2660003f788fcdd25bed88691e8abfcd3b65163ad6.8a4cf3d1cf24fa5b4706391a61020318e4695fd7576f4fcad409a8cc13c4f6dd",
+  refreshToken: "",
 
   // 想作为画廊首页的 115 文件夹 ID。留空表示使用网盘根目录。
   rootFolderId: "",
